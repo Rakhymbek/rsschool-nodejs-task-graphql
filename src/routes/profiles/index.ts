2d3579d32352cb3c486e19f47b8b317d588ed15f
Rakhymbek Sagyn <sagrakha@gmail.com>
2023-01-28 18:59:59 +0600

    feat: add logic to profiles

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -6,9 +6,9 @@ import type { ProfileEntity } from '../../utils/DB/entities/DBProfiles';
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<
-    ProfileEntity[]
-  > {});
+  fastify.get('/', async function (request, reply): Promise<ProfileEntity[]> {
+    return await fastify.db.profiles.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +17,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params;
+      const profile = await fastify.db.profiles.findOne({
+        key: 'id',
+        equals: id,
+      });
+      return profile ? profile : reply.code(404);
+    }
   );
 
   fastify.post(
@@ -27,7 +34,16 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: createProfileBodySchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const body = request.body;
+      const profile = await fastify.db.profiles.findOne({
+        key: 'userId',
+        equals: body.userId,
+      });
+      if (profile) return reply.code(400);
+      const newProfile = await fastify.db.profiles.create(body);
+      return newProfile ? newProfile : reply.code(400);
+    }
   );
 
   fastify.delete(
@@ -37,7 +53,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params;
+      try {
+        return await fastify.db.profiles.delete(id);
+      } catch (error) {
+        return reply.code(400);
+      }
+    }
   );
 
   fastify.patch(
@@ -48,7 +71,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params;
+      try {
+        return await fastify.db.profiles.change(id, request.body);
+      } catch (error) {
+        return reply.code(400);
+      }
+    }
   );
 };
 
