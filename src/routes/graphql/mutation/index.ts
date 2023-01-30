e9012a5cdb710f3cb3d1541b2ebf3bbe4c07ee90
Rakhymbek Sagyn <sagrakha@gmail.com>
2023-01-30 22:56:45 +0600

    feat: add profile, post, member-type resolvers to mutation-schema

--- src/routes/graphql/mutation/index.ts
+++ src/routes/graphql/mutation/index.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import { GraphQLObjectType } from 'graphql';
-import { userCreateType, userUpdateType } from '../types/inputDataTypes';
-import { userDataType } from '../types/userDataTypes';
+import { memberTypeUpdateType, postCreateType, postUpdateType, profileCreateType, profileUpdateType, userCreateType, userUpdateType } from '../types/inputDataTypes';
+import { memberTypeDataType, postDataType, profileDataType, userDataType } from '../types/userDataTypes';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
@@ -22,9 +22,79 @@ export default new GraphQLObjectType({
       },
       resolve: async (_, { user }, fastify: FastifyInstance) => {
         const { json } = await fastify.inject({
-          url: `users/${user.id}`,
+          url: `/users/${user.id}`,
           method: 'PATCH',
           payload: user,
+        });
+        return json();
+      },
+    },
+    createProfile: {
+      type: profileDataType,
+      args: {
+        profile: { type: profileCreateType },
+      },
+      resolve: async (_, { profile }, fastify: FastifyInstance) => {
+        const { json } = await fastify.inject({
+          url: `/profiles`,
+          method: 'POST',
+          payload: profile,
+        });
+        return json();
+      },
+    },
+    updateProfile: {
+      type: profileDataType,
+      args: {
+        profile: { type: profileUpdateType },
+      },
+      resolve: async (_, { profile }, fastify: FastifyInstance) => {
+        const { json } = await fastify.inject({
+          url: `/profiles/${profile.id}`,
+          method: 'PATCH',
+          payload: profile,
+        });
+        return json();
+      },
+    },
+    createPost: {
+      type: postDataType,
+      args: {
+        post: { type: postCreateType },
+      },
+      resolve: async (_, { post }, fastify: FastifyInstance) => {
+        const { json } = await fastify.inject({
+          url: `/posts`,
+          method: 'POST',
+          payload: post,
+        });
+        return json();
+      },
+    },
+    updatePost: {
+      type: postDataType,
+      args: {
+        post: { type: postUpdateType },
+      },
+      resolve: async (_, { post }, fastify: FastifyInstance) => {
+        const { json } = await fastify.inject({
+          url: `/posts/${post.id}`,
+          method: 'PATCH',
+          payload: post,
+        });
+        return json();
+      },
+    },
+    updateMemberType: {
+      type: memberTypeDataType,
+      args: {
+        memberType: { type: memberTypeUpdateType },
+      },
+      resolve: async (_, { memberType }, fastify: FastifyInstance) => {
+        const { json } = await fastify.inject({
+          url: `/member-types/${memberType.id}`,
+          method: 'PATCH',
+          payload: memberType,
         });
         return json();
       },
